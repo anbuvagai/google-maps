0d1da8fd0353ad7048409df3105a270c5ffa779f
anbuvagai <anbuvagai@gmail.com>
2025-01-07 08:59:24 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Button } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import Geolocation from "react-native-geolocation-service";
+import * as Linking from "expo-linking";
 
-export default function App() {
+const App = () => {
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const destination = {
+    latitude: 9.881917793666702,
+    longitude: 78.07049882711758,
+  }; // Example destination coordinates
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setCurrentLocation({});
+      },
+      (error) => {
+        console.error(error);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: 9.881917793666702,
+          longitude: 78.07049882711758,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {currentLocation && (
+          <Marker coordinate={currentLocation} title="You are here" />
+        )}
+        <Marker coordinate={destination} title="Destination" />
+      </MapView>
+      <Button
+        title="Start Navigation"
+        onPress={() => {
+          const url = `https://www.google.com/maps/dir/?api=1&destination=${destination.latitude},${destination.longitude}`;
+          Linking.openURL(url);
+        }}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: "100%",
+    height: "97%",
   },
 });
+
+export default App;
